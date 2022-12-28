638baf4d5fd72a3a7752f14f8cec84cc23f3031b
Hashan_Piyumal <hashan8991@gmail.com>
2022-12-28 15:33:43 +0530

    Initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Cras condimentum felis vel vehicula varius nisl sem dapibus magna tristique faucibus lectus ex in lacus",
+    },
+    {
+        id: 2,
+        rating: 8,
+        text: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Cras condimentum felis vel vehicula varius nisl sem dapibus magna tristique faucibus lectus ex in lacus",
+    },
+    {
+        id: 3,
+        rating: 7,
+        text: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Cras condimentum felis vel vehicula varius nisl sem dapibus magna tristique faucibus lectus ex in lacus",
+    },
+    {
+        id: 4,
+        rating: 5,
+        text: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Cras condimentum felis vel vehicula varius nisl sem dapibus magna tristique faucibus lectus ex in lacus",
+    }
+]
+
+export default FeedbackData;
