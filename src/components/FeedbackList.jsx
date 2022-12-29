8e639377474f23f87dcc6e2d89a6a6b9c32e8311
Hashan_Piyumal <hashan8991@gmail.com>
2022-12-29 20:18:20 +0530

    Create a spinner componet for loading

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,14 +3,15 @@ import { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
 //import PropTypes from "prop-types"
 import FeedbackContext from "./context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList( {handleDelete} ) {
-    const {feedback} = useContext(FeedbackContext)
+    const {feedback, isLoading} = useContext(FeedbackContext)
     
-    if ( !feedback || feedback.length === 0){  
+    if ( !isLoading && (!feedback || feedback.length === 0)){  
         return <p> No feedback yet </p>
     }
-    return (
+    return isLoading ? <Spinner /> : (
         <div className = "feedback-list"> 
             <AnimatePresence>
                 { feedback.map((item) => (
@@ -25,7 +26,23 @@ function FeedbackList( {handleDelete} ) {
             </AnimatePresence>
             
         </div>
-    );
+    )
+    // return (
+    //     <div className = "feedback-list"> 
+    //         <AnimatePresence>
+    //             { feedback.map((item) => (
+    //                 <motion.div key={item.id} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}> 
+    //                     <FeedbackItem 
+    //                         key = {item.id} 
+    //                         item = {item} 
+    //                     />
+    //                 </motion.div>
+                    
+    //             ))}
+    //         </AnimatePresence>
+            
+    //     </div>
+    // );
     /*    return (
             <div className = "feedback-list"> 
                 { feedback.map((item) => (
